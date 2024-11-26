1bc59e3c0c34c5b77e15bab3525cc6a999800bdc
Evan Purkhiser <evanpurkhiser@gmail.com>
2024-11-25 21:29:00 -0500

    A bunch of cleanup

--- index.ts
+++ index.ts
@@ -1,37 +1,37 @@
-import fs from "fs";
-import signale from "signale";
-import "@sentry/tracing";
+import fs from 'fs';
+import signale from 'signale';
+import '@sentry/tracing';
 
-import { MixstatusProcessor, bringOnline } from "prolink-connect";
+import {MixstatusProcessor, bringOnline} from 'prolink-connect';
 
 async function writeNowPlayingToFile() {
-  signale.await("Bringing up prolink network");
+  signale.await('Bringing up prolink network');
   const network = await bringOnline();
-  signale.success("Network online, preparing to connect");
+  signale.success('Network online, preparing to connect');
 
-  network.deviceManager.on("connected", (d) =>
-    signale.star("New device: %s [id: %s]", d.name, d.id)
+  network.deviceManager.on('connected', d =>
+    signale.star('New device: %s [id: %s]', d.name, d.id)
   );
 
-  signale.await("Autoconfiguring network.. waiting for devices");
+  signale.await('Autoconfiguring network.. waiting for devices');
   await network.autoconfigFromPeers();
-  signale.await("Autoconfigure successfull!");
+  signale.await('Autoconfigure successfull!');
 
-  signale.await("Connecting to network!");
+  signale.await('Connecting to network!');
   network.connect();
 
   if (!network.isConnected()) {
-    signale.error("Failed to connect to the network");
+    signale.error('Failed to connect to the network');
     return;
   }
 
-  signale.star("Network connected! Network services initalized");
+  signale.star('Network connected! Network services initalized');
 
   const processor = new MixstatusProcessor();
-  network.statusEmitter.on("status", (s) => processor.handleState(s));
+  network.statusEmitter.on('status', s => processor.handleState(s));
 
-  processor.on("nowPlaying", async (state) => {
-    const { trackDeviceId, trackId, trackSlot, trackType } = state;
+  processor.on('nowPlaying', async state => {
+    const {trackDeviceId, trackId, trackSlot, trackType} = state;
 
     const track = await network.db.getMetadata({
       trackId,
@@ -45,7 +45,7 @@ async function writeNowPlayingToFile() {
 
     signale.note(`New track now playing! ${trackName}`);
 
-    fs.writeFileSync("now_playing.txt", trackName);
+    fs.writeFileSync('now_playing.txt', trackName);
   });
 }
 
